7b70dcae06592b559f44f1359127556a04535c8f
yelkov <yelkovq@gmail.com>
2024-05-03 20:47:32 +0200

    feat(AgedBrie.java): implementar AgedBrie

--- app/src/main/java/edu/badpals/gildedrose/AgedBrie.java
+++ app/src/main/java/edu/badpals/gildedrose/AgedBrie.java
@@ -1,5 +1,51 @@
 package edu.badpals.gildedrose;
 
-public class AgedBrie {
+public class AgedBrie implements Updateable{
+    private Item item;
+
+    public AgedBrie(String name, int sellIn, int quality){
+        this.item = new Item( name, sellIn, quality);
+    }
+
+
+    public String getName() {
+        return item.name;
+    }
+
+    public int getQuality() {
+        return item.quality;
+    }
+
+    @Override
+    public int getSellIn() {
+        return item.sellIn;
+    }
+
+    @Override
+    public void setSellIn(int newSellIn) {
+        this.item.sellIn = newSellIn;
+    }
+
+    public void setName(String newName) {
+        this.item.name = newName;
+    }
+    public void setQuality(int newQuality) {
+        this.item.quality = newQuality;
+    }
+
+    @Override
+    public void updateQuality() {
+        if(getSellIn()>=0){
+            setQuality(getQuality()+1);
+        }else{
+            setQuality(getQuality()+2);
+        }
+        if(getQuality()>50){
+            setQuality(50);
+        }
+    }
+
+
+
 
 }
