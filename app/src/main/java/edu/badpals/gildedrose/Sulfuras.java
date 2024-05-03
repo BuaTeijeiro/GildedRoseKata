5fe08d4e2d851914a2cdc5b3464d6a3d798a9159
BuaTeijeiro <bua.teijeiro.david@gmail.com>
2024-05-03 21:14:29 +0200

    feat(Sulfuras.java): Implementar Clase Sulfuras

---
+++ app/src/main/java/edu/badpals/gildedrose/Sulfuras.java
@@ -0,0 +1,39 @@
+package edu.badpals.gildedrose;
+
+public class Sulfuras implements Updateable{
+    private Item item;
+
+    public Sulfuras(String name, int SellIn){
+        this.item = new Item(name, SellIn, 80);
+    }
+
+    public String getName(){
+        return this.item.name;
+    }
+
+    public int getSellIn(){
+        return this.item.sellIn;
+    }
+
+    public int getQuality(){
+        return this.item.quality;
+    }
+
+    public void setName(String newName){
+        this.item.name = newName;
+    }
+
+    public void setSellIn(int newSellIn){
+        this.item.sellIn = newSellIn;
+    }
+
+    @Override
+    public void updateQuality() {
+        //Esto nunca se modifica
+    }
+
+    @Override
+    public void updateSellIn(){
+        //Esto nunca se modifica
+    }
+}
