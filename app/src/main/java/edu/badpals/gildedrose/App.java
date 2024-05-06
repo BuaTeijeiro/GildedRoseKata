799c155c3a1bf7cd15b3739e0fa00d52f5c70d57
yelkov <yelkovq@gmail.com>
2024-05-06 09:49:26 +0200

    feat(GildedRose): implementar main - App

--- app/src/main/java/edu/badpals/gildedrose/App.java
+++ app/src/main/java/edu/badpals/gildedrose/App.java
@@ -2,6 +2,26 @@ package edu.badpals.gildedrose;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+    GildedRose shop = new GildedRose();
+
+
+        shop.addItem(new NormalItem("+5 Dexterity Vest", 10, 20));
+        shop.addItem(new AgedBrie("Aged Brie", 2, 0));
+        shop.addItem(new NormalItem("Elixir of the Mongoose", 5, 7));
+        shop.addItem(new Sulfuras("Sulfuras, Hand of Ragnaros", 0));
+        shop.addItem(new Sulfuras("Sulfuras, Hand of Ragnaros", -1));
+        shop.addItem(new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        shop.addItem(new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+        shop.addItem(new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 5, 49));
+        shop.addItem(new Conjured("Conjured Mana Cake", 3, 6));
+
+        System.out.println("-------- day 0 --------\n");
+        System.out.println(shop);
+
+        for(int i = 1;i < 30; i++ ){
+            shop.updateItems();
+            System.out.println(String.format("-------- day %d --------%n", i));
+            System.out.println(shop);
+        }
     }
 }
